04ec0800ec6c354411ef439b81a1ec5a4a9c073a
edwardanchundia <edward.anchundia@gmail.com>
2016-10-31 11:57:27 -0400

    Homework library congress

---
+++ LibraryOfCongress/LibraryOfCongress/CongressDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CongressDetailViewController.swift
+//  LibraryOfCongress
+//
+//  Created by Edward Anchundia on 10/31/16.
+//  Copyright © 2016 Edward Anchundia. All rights reserved.
+//
+
+import UIKit
+
+class CongressDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ LibraryOfCongress/LibraryOfCongress/ImageExtension.swift
@@ -0,0 +1,33 @@
+//
+//  ImageExtension.swift
+//  LibraryOfCongress
+//
+//  Created by Edward Anchundia on 10/31/16.
+//  Copyright © 2016 Edward Anchundia. All rights reserved.
+//
+
+import UIKit
+
+let cacheImage = NSCache<NSString, UIImage>()
+extension UIImageView {
+    func downloadImage(urlString: String) {
+        if let cachedImaged = cacheImage.object(forKey: urlString as NSString) {
+            self.image = cachedImaged
+            return
+        }
+        guard let url = URL(string: urlString) else { return }
+        var imageData: Data?
+        DispatchQueue.global(qos: .background).async {
+            do {
+                imageData = try Data(contentsOf: url)
+            } catch let err {
+                print("imageData error ==> \n\(err)")
+            }
+            guard let data = imageData, let downloadedImage = UIImage(data: data) else { return }
+            cacheImage.setObject(downloadedImage, forKey: urlString as NSString)
+            DispatchQueue.main.async {
+                self.image = UIImage(data: data)
+            }
+        }
+    }
+}
